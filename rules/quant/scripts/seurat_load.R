6cb80c738bc16e440c510dbf0301d363c7d6f707
flatberg <arnar.flatberg@ntnu.no>
2018-12-04 17:43:38 +0100

    D11

--- rules/quant/scripts/seurat_load.R
+++ rules/quant/scripts/seurat_load.R
@@ -1,14 +1,15 @@
-#/usr/bin/env Rscript
+#!/usr/bin/env Rscript
+
 suppressPackageStartupMessages(require(Seurat))
 
+if (!require(Seurat)) {
+    stop("This script needs to have Seurat installed.")
+}
 if (!require(argparse)) {
     install.packages("argparse", repos="http://cran.rstudio.com")
     library("argparse")
 }
-if (!require(Seurat)) {
-    install.packages("Seurat", repos="http://cran.rstudio.com")
-    library("Seurat")
-}
+
 
 if (!require(Matrix)) {
     install.packages("Matrix", repos="http://cran.rstudio.com")
@@ -17,9 +18,9 @@ if (!require(Matrix)) {
 
 
 parser <- ArgumentParser()
-parser$add_argument("-i", "--input", default="data/tmp/test_sample/outs/filtered_gene_bc_matrices",
-                    help="Directory containing the matrix.mtx, genes.tsv, and barcodes.tsv files provided by 10X. A list can be given in order to load several data directories")
-parser$add_argument("-s", "--sample-info",
+parser$add_argument("-i", "--input", default="data/tmp/test_sample/outs/filtered_gene_bc_matrices", nargs="+",
+                    help="Directory containing the matrix.mtx.gz, genes.tsv.gz, and barcodes.tsv.gz files provided by 10X. Multiple dirs allowed.")
+parser$add_argument("-s", "--sample-info", dest="samples", default=NULL,
                     help="Sample info. Tab delimited file, needs column with `sample_id`")
 parser$add_argument("-o", "--output", default="data/processed/test_sample/seurat_obj.rds",
                     help="Output filename (rds file)")
@@ -29,6 +30,24 @@ parser$add_argument("-v", "--verbose", action="store_true",
 args <- parser$parse_args(args=commandArgs(TRUE))
 if (args$verbose == TRUE) options(echo=TRUE)
 
+if (length(args$input)> 1){
+    input.names <- NULL
+    for (dir in args$input){
+        parent.dir <- dirname(dir)
+        if (basename(parent.dir) == "outs"){
+            parent.dir <- dirname(parent.dir)
+        }
+        input.names <- c(input.names, basename(parent.dir))
+    }
+    names(args$input) <- input.names
+}
+
 data <- Read10X(args$input)
-seurat.obj  <- CreateSeuratObject(raw.data=data)
-saveRDS(seurat.obj, out.fn)
+if (!is.null(args$samples)){
+    meta.info <- read.delim(args$samples, sep="\t", row.names=1)
+} else{
+    meta.info <- NULL
+}
+
+seurat.obj  <- CreateSeuratObject(data, meta.data = meta.info, assay="RNA", names.delim="_")
+saveRDS(seurat.obj, args$output)
