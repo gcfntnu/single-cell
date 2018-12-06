ae9dec1f47bea96004eb2fee94c7b0b69f83632b
flatberg <arnar.flatberg@ntnu.no>
2018-12-06 16:30:40 +0100

    13-3

---
+++ rules/quant/scripts/alevin_seurat.R
@@ -0,0 +1,36 @@
+library(Seurat)
+library(argparse)
+
+# Parts of the function is taken from Seurat's Read10x parsing function
+ReadAlevin <- function( base.path = NULL ){
+    if (! dir.exists(base.path )){
+      stop("Directory provided does not exist")
+    }
+
+    barcode.loc <- paste0( base.path, "alevin/quants_mat_rows.txt" )
+    gene.loc <- paste0( base.path, "alevin/quants_mat_cols.txt" )
+    matrix.loc <- paste0( base.path, "alevin/quants_mat.csv" )
+    if (!file.exists( barcode.loc )){
+      stop("Barcode file missing")
+    }
+    if (! file.exists(gene.loc) ){
+      stop("Gene name file missing")
+    }
+    if (! file.exists(matrix.loc )){
+      stop("Expression matrix file missing")
+    }
+    matrix <- as.matrix(read.csv( matrix.loc, header=FALSE))
+    matrix <- t(matrix[,1:ncol(matrix)-1])
+
+    cell.names <- readLines( barcode.loc )
+    gene.names <- readLines( gene.loc )
+
+    colnames(matrix) <- cell.names
+    rownames(matrix) <- gene.names
+    matrix[is.na(matrix)] <- 0
+    return(matrix)
+}
+
+sdata <- CreateSeuratObject(alv.data, min.cells = 3, project="10X_GCF")
+
+saveRDS(sdata, output.path)
