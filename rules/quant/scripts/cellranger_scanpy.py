0f50bdec495f977e17a0fd7e952f9b85dab28f8e
flatberg <arnar.flatberg@ntnu.no>
2019-03-05 13:21:47 +0100

    G2

--- rules/quant/scripts/cellranger_scanpy.py
+++ rules/quant/scripts/cellranger_scanpy.py
@@ -12,10 +12,11 @@ import scanpy.api as sc
 GENOME = {'homo_sapiens': 'GRCh38',
           'human': 'GRCh38',
           'hg38': 'GRCh38',
+          'GRCh38': 'GRCh38',
           'mus_musculus': 'mm10',
           'mouse': 'mm10',
           'mm10': 'mm10',
-          '': ''
+          'GRCm38': 'mm10'
 }
 
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -25,12 +26,12 @@ parser.add_argument('-o', '--outfile', help="output filename",)
 parser.add_argument('-f', '--format', choices=['anndata', 'loom', 'csvs'], default='anndata', help="output file format")
 parser.add_argument('--sample-sheet', help="samplesheet filename")
 parser.add_argument('--genome', help="Reference genome", default=None)
+parser.add_argument('--filter-org', help="Filter data (genes) by organism", default=None)
 
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    print(args)
     
     if not os.path.exists(args.infile):
         raise IOError
@@ -40,8 +41,14 @@ if __name__ == '__main__':
             raise ValueError('loading a .h5 file in scanpy requires the `genome` parameter')
         data = sc.read_10x_h5(args.infile, genome=args.genome)
     else:
-        data = sc.read_10x_mtx(args.infile, gex_only=False, var_names='gene_ids')
-    data.var_names_make_unique()
+        data = sc.read_10x_mtx(args.infile, gex_only=True, var_names='gene_ids')
+
+
+    if args.filter_org is not None:
+        org = args.filter_org
+        if org in GENOME:
+            org = GENOME[org]
+        PREFIX = {'GRCh38': 'hg', 'GRCm38':'', 'mm10':''}
     
     if args.format == 'anndata':
         data.write(args.outfile)
